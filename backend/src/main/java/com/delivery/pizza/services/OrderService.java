17d7850fe3e54c2d5d3091e5248e7c03105c7045
Joshua <josh_andrademota@hotmail.com>
2021-01-04 16:05:44 -0300

    GET /orders

--- backend/src/main/java/com/delivery/pizza/services/OrderService.java
+++ backend/src/main/java/com/delivery/pizza/services/OrderService.java
@@ -1,8 +1,25 @@
 package com.delivery.pizza.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.delivery.pizza.dto.OrderDTO;
+import com.delivery.pizza.entities.Order;
+import com.delivery.pizza.repositories.OrderRepository;
 
 @Service
 public class OrderService {
 
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		List<Order> list = orderRepository.findOrdersWithProducts();
+		return list.stream().map(order -> new OrderDTO(order)).collect(Collectors.toList());
+	}
 }
