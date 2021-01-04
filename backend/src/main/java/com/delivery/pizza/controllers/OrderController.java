acd8bb40ca91ae55479b1d40e8598e1231764306
Joshua <josh_andrademota@hotmail.com>
2021-01-04 16:35:38 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/com/delivery/pizza/controllers/OrderController.java
+++ backend/src/main/java/com/delivery/pizza/controllers/OrderController.java
@@ -6,14 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.delivery.pizza.dto.OrderDTO;
-import com.delivery.pizza.entities.Order;
 import com.delivery.pizza.services.OrderService;
 
 @RestController
@@ -31,8 +32,14 @@ public class OrderController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public OrderDTO abrirPedido(@RequestBody OrderDTO orderDTO) {
+	public OrderDTO insert(@RequestBody OrderDTO orderDTO) {
 		return orderService.insert(orderDTO);
+	}
+	
+	@PutMapping("/{id}/delivered")
+	public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id) {
+		OrderDTO orderDTO = orderService.setDelivered(id);
+		return ResponseEntity.ok().body(orderDTO);
 	}
 	
 }
