4b9c00bd7c1683350111274cadf1b580a214ab5b
Joshua <josh_andrademota@hotmail.com>
2021-01-04 15:05:19 -0300

    GET/products

---
+++ backend/src/main/java/com/delivery/pizza/repositories/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.delivery.pizza.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.delivery.pizza.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	public List<Product> findAllByOrderByNameAsc();
+}
---
+++ backend/src/main/java/com/delivery/pizza/services/OrderService.java
@@ -0,0 +1,8 @@
+package com.delivery.pizza.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+}
