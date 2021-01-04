604df0ebe2fbc627598e49d48cab0c0a7607fdcf
Joshua <josh_andrademota@hotmail.com>
2021-01-04 16:26:34 -0300

    POST /orders

--- backend/src/main/java/com/delivery/pizza/controllers/OrderController.java
+++ backend/src/main/java/com/delivery/pizza/controllers/OrderController.java
@@ -3,12 +3,17 @@ package com.delivery.pizza.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.delivery.pizza.dto.OrderDTO;
+import com.delivery.pizza.entities.Order;
 import com.delivery.pizza.services.OrderService;
 
 @RestController
@@ -22,6 +27,12 @@ public class OrderController {
 	public ResponseEntity<List<OrderDTO>> findAll(){
 		List<OrderDTO> list = orderService.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public OrderDTO abrirPedido(@RequestBody OrderDTO orderDTO) {
+		return orderService.insert(orderDTO);
 	}
 	
 }
--- backend/src/main/java/com/delivery/pizza/services/OrderService.java
+++ backend/src/main/java/com/delivery/pizza/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.delivery.pizza.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.delivery.pizza.dto.OrderDTO;
+import com.delivery.pizza.dto.ProductDTO;
 import com.delivery.pizza.entities.Order;
+import com.delivery.pizza.entities.OrderStatus;
+import com.delivery.pizza.entities.Product;
 import com.delivery.pizza.repositories.OrderRepository;
+import com.delivery.pizza.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,9 +22,23 @@ public class OrderService {
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){
 		List<Order> list = orderRepository.findOrdersWithProducts();
 		return list.stream().map(order -> new OrderDTO(order)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		for(ProductDTO productDTO : dto.getProducts()) {
+			Product product = productRepository.getOne(productDTO.getId());
+			order.getProducts().add(product);
+		}
+		order = orderRepository.save(order);
+		return new OrderDTO(order);
 	}
 }
