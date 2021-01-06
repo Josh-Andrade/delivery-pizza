e09940c2703ef66cd1dcdd62fedd1d96e33f7260
Joshua <josh_andrademota@hotmail.com>
2021-01-06 14:26:23 -0300

    add axios and dynamic products from api

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -8,10 +8,10 @@ function Footer(){
         <footer className="main-footer">
             App desenvolvido durante a 2ª edição do evento Semana DevSuperior
             <div className="footer-icons">
-                <a href="" target="_new">
+                <a href="vazio" target="_new">
                     <YoutubeIcon/>
                 </a>
-                <a href="" target="_new">
+                <a href="vazio" target="_new">
                     <InstagramIcon/>
                 </a>
                 <a href="https://www.linkedin.com/in/joshua-andrade-mota-de-almeida/" target="_new">
