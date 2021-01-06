a0d753c533d05d34401ecf09ce72e8fab99bf51c
Joshua <josh_andrademota@hotmail.com>
2021-01-06 13:23:02 -0300

    add routes and stepsHeader

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -1,11 +1,12 @@
 import './styles.css'
 import {ReactComponent as Logo} from './Logo.svg'
+import { Link } from 'react-router-dom';
 
 function NavBar(){
     return(
         <nav className="main-navbar">
             <Logo/>
-            <a href="home" className="logo-text">Pizza Delivery</a>
+            <Link to="/" className="logo-text">Pizza Delivery</Link>
         </nav>
     )
 }
